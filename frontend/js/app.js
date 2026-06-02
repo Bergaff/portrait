de6a9b99a9594c56d1847bae7a936c803aa213fd
Bergaff <51030407+Bergaff@users.noreply.github.com>
2026-06-02 08:56:15 +0300

    Update frontend/js/app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,6 +1,6 @@
 // ========== SUPABASE ==========
 // ========== ВЕРСИЯ ==========
-const APP_VERSION = "0.0292";
+const APP_VERSION = "0.0294";
 
 // ========== SUPABASE ==========
 const { createClient } = supabase;
